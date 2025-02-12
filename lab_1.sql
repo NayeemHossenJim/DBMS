8db2932fc68c758fab8a7d91ff72abcac8edbc1c
NayeemHossenJim <155984091+NayeemHossenJim@users.noreply.github.com>
2025-02-13 01:14:59 +0600

    Update lab_1.sql

--- lab_1.sql
+++ lab_1.sql
@@ -1,10 +1,1 @@
-insert into Director
-values  (1,"Zahir Raihan",1935,5,5),
-		(2,"RajKumar Hirani",1962,5,9),
-        (3,"Saytajit Ray",1921,45,55),
-        (4,"Anjan Dutt",1953,23,17),
-        (5,"Rituparno Ghosh",1963,20,15),
-        (6,"Goutam Ghose",1950,12,8),
-        (7,"Aparna Sen",1945,12,7),
-        (8,"Kaushik Ganguly",1968,23,30);
-select * from Director
+select * from actor
