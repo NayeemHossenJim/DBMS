5ac31733a1cf01f954c1eebadeb204133595c18c
NayeemHossenJim <155984091+NayeemHossenJim@users.noreply.github.com>
2025-02-16 21:33:18 +0600

    Update lab_1.sql

--- lab_1.sql
+++ lab_1.sql
@@ -1,3 +1,4 @@
-select movie_name,genre
-from movie 
-where Movie_name Like "A%";
+select movie_name,IMDB_Rating,director_name
+from movie inner join director
+on movie.director_id = director.person_id
+where movie_name like "__________%"
